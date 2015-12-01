f45b18de578ad40a2fafb222a8917d8c3d30dd1c
Ppslavsky <m.poslavski@napishem.com>
2015-12-01 15:22:04 +0300

    add Button component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Button from './Button';
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+			<div>
+      <h3>При небольшом количестве кнопок</h3>
+			<Button href='#'>Default</Button>
+			<Button green>Color</Button>
+			<Button large>Size</Button>
+			<Button green large>Color Size</Button>
+			</div>
     );
   }
 }
---
+++ src/Button.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+
+export default class Button extends Component {
+	propTypes: {
+		disabled: React.PropTypes.bool,
+		compact: React.PropTypes.bool,
+		primary: React.PropTypes.bool,
+		scary: React.PropTypes.bool,
+		type: React.PropTypes.string,
+		href: React.PropTypes.string,
+		onClick: React.PropTypes.func
+	}
+
+	static defaultProps = {
+		disabled: false,
+		type: 'button',
+		//onClick: noop
+	}
+
+
+
+  render() {
+		const element = this.props.href ? 'a' : 'button';
+		const buttonClasses = classNames( {
+			'btn': true,
+			'waves-effect': true,
+			'waves-light': true,
+			'btn-large': this.props.large,
+			'green lighten-2': this.props.green,
+			'red lighten-2': this.props.green,
+			'blue lighten-2': this.props.green
+		} );
+		const props = Object.assign( {}, this.props, {
+				className: classNames( this.props.className, buttonClasses )
+		} );
+
+    return (
+			React.createElement( element, props, this.props.children )
+    );
+  }
+}
+
+export default Button;
